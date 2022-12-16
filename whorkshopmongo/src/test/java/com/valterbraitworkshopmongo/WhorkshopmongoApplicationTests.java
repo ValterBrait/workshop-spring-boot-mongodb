e092f13f6b3144c38726f2b25b41dff002ee259f
ValterBrait <valter.brait@outlook.it>
2022-12-16 08:32:19 -0400

    Inicio

---
+++ whorkshopmongo/src/test/java/com/valterbraitworkshopmongo/WhorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valterbraitworkshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
